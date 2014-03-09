ed5a3ecbd36481ba7f37b8001a8c71c7d80385d7
NehaMadiwal <nehamadiwal@gmail.com>
2014-03-09 22:23:20 +0530

    first commit

---
+++ app/src/main/java/com/vxtindia/app/activity/MainActivity.java
@@ -0,0 +1,34 @@
+package com.vxtindia.app.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends Activity {
+
+    String url="";
+    ImageButton play;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        play = (ImageButton)findViewById(R.id.play);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,VideoPlayerActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+
+}
